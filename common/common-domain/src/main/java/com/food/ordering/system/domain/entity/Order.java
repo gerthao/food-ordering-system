7ef94759f4604661ae36dca3cad30db065add3b7
Ger Thao <g.thao19@gmail.com>
2023-06-03 17:54:59 -0500

    add pay, approve, initCancel, cancel operations for Order class.

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/Order.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/Order.java
@@ -34,6 +34,39 @@ public class Order extends AggregateRoot<OrderId> {
         return new Builder();
     }
 
+    public void pay() {
+        if (orderStatus != OrderStatus.PENDING)
+            throw new OrderDomainException("Order is not in correct state for pay operation!");
+        orderStatus = OrderStatus.PAID;
+    }
+
+    public void approve() {
+        if (orderStatus != OrderStatus.PAID)
+            throw new OrderDomainException("Order is not in correct state for approve operation!");
+        orderStatus = OrderStatus.APPROVED;
+    }
+
+    public void initCancel(List<String> failureMessages) {
+        if (orderStatus != OrderStatus.PAID)
+            throw new OrderDomainException("Order is not in correct state for initCancel operation!");
+        orderStatus = OrderStatus.CANCELLING;
+        updateFailureMessages(failureMessages);
+    }
+
+    public void cancel(List<String> failureMessages) {
+        if (!(orderStatus == OrderStatus.CANCELLING || orderStatus == OrderStatus.PENDING))
+            throw new OrderDomainException("Order is not in correct state for cancel operation!");
+        orderStatus = OrderStatus.CANCELLED;
+        updateFailureMessages(failureMessages);
+    }
+
+    private void updateFailureMessages(List<String> failureMessages) {
+        if (this.failureMessages != null && failureMessages != null)
+            this.failureMessages.addAll(failureMessages.stream().filter(m -> !m.isEmpty() && !m.isBlank()).toList());
+
+        if (this.failureMessages == null) this.failureMessages = failureMessages;
+    }
+
     public void initializeOrder() {
         setId(new OrderId(UUID.randomUUID()));
         trackingId  = new TrackingId(UUID.randomUUID());
