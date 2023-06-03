8425fff9ab211e6a37802d980534952171344317
Ger Thao <g.thao19@gmail.com>
2023-06-03 15:52:36 -0500

    add value objects for customers, orders, and restaurants

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,60 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+import java.util.function.Function;
+
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public boolean isGreaterThanZero() {
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(Money money) {
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    public Money add(Money money) {
+        return operation(money, this.amount::add);
+    }
+
+    public Money subtract(Money money) {
+        return operation(money, this.amount::subtract);
+    }
+
+    public Money multiply(Money money) {
+        return operation(money, this.amount::multiply);
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    private BigDecimal setScale(BigDecimal input) {
+        return input.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    private Money operation(Money money, Function<BigDecimal, BigDecimal> op) {
+        var resultingAmount = setScale(op.apply(money.getAmount()));
+        return new Money(resultingAmount);
+    }
+}
