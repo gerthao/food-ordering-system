de278dfa16393c6321d9f93df339c951dacc2f22
Ger Thao <g.thao19@gmail.com>
2023-06-05 06:35:09 -0500

    add application event publisher for domain events and an application listener for order create events

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ApplicationDomainEventPublisher.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.event.publisher.DomainEventPublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ApplicationDomainEventPublisher implements ApplicationEventPublisherAware, DomainEventPublisher<OrderCreatedEvent> {
+
+    private ApplicationEventPublisher eventPublisher;
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.eventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public void publish(OrderCreatedEvent domainEvent) {
+        this.eventPublisher.publishEvent(domainEvent);
+        log.info("OrderCreateEvent is published for order id : {}", domainEvent.getOrder().getId().getValue());
+    }
+}
