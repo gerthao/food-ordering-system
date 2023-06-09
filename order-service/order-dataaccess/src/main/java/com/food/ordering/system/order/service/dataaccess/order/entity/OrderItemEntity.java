bb56317b726879b2335bbccd384b3d85a8f07b50
Ger Thao <g.thao19@gmail.com>
2023-06-08 23:42:39 -0500

    add order entity classes for order data access module.

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntity.java
@@ -0,0 +1,42 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Table(name = "orders_items")
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(OrderItemEntityId.class)
+public class OrderItemEntity {
+    @Id
+    private Long id;
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
+    private OrderEntity order;
+    private UUID productId;
+    private BigDecimal price;
+    private Integer quantity;
+    private BigDecimal subtotal;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntity that = (OrderItemEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order);
+    }
+}
